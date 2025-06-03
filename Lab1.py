db780bc0ebb6fa873f0fd08f9bfedbfd57509cb3
Abenezer <abnzr.dev@gmail.com>
2025-06-03 12:35:30 +0500

    Python Basics added(Printing, Comments, Variables)

---
+++ Lab1.py
@@ -0,0 +1,91 @@
+# First Assignment
+# Abenezer Alemayehu Lemma
+# 6/3/2025
+
+# Part 1: Python Basics
+
+# Basic print statement
+print("Hello, World!")  # This will print Hello, World
+
+# Checking Python version
+import sys
+print(sys.version)
+
+# Simple conditional statements
+if 5 > 2:
+    print("Five is greater than two!")
+
+if 5 > 2:
+    print("Five is greater than two!")
+if 5 > 2:
+    print("Five is greater than two!")
+
+# Variables and data types
+x = 5
+y = "Hello, World!"
+
+# Single-line comment
+# This is a comment.
+print("Hello, World!")
+
+# Multi-line comment
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+# Assigning different data types to variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+# Type conversion
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+# Checking data types
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+# Strings with different quotation marks
+x = "John"
+x = 'John'  # Same as above
+
+# Variable names are case-sensitive
+a = 4
+A = "Sally"  # A will not overwrite a
+
+# Valid variable names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# Naming conventions
+myVariableName = "John"  # Camel case
+MyVariableName = "John"  # Pascal case
+my_variable_name = "John"  # Snake case
+
+# Assigning the same value to multiple variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+# Assigning multiple values to multiple variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
