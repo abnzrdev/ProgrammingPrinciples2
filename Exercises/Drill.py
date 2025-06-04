7eb09d07e310e1fe8958f32217d66331cc0edb52
Abenezer <abnzr.dev@gmail.com>
2025-06-04 10:09:28 +0500

    Password Taking Input Updated

--- Exercises/Drill.py
+++ Exercises/Drill.py
@@ -13,6 +13,6 @@ print(total)
 # Password Checker
 inp = input("Enter your password")
 while(inp != "admin123"):
-    inp = input("Enter your password")
+    inp = input("Enter your password : ")
 
 print("Correct Password")
