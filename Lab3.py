48efff8b8890f5ce2a390977243be1bd3724441f
Abenezer <abnzr.dev@gmail.com>
2025-06-10 01:11:01 +0500

    Add examples of class usage and object manipulation in Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -146,3 +146,70 @@ mydoubler = myfunc(2)
 mytripler = myfunc(3)
 print(mydoubler(11))
 print(mytripler(11))
+
+# Simple class with one property
+class MyClass:
+    x = 5
+
+p1 = MyClass()
+print(p1.x)
+
+# Class with __init__ constructor
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+p1 = Person("John", 36)
+print(p1.name)
+print(p1.age)
+
+# Printing an object without __str__ method
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+p1 = Person("John", 36)
+print(p1)
+
+# Printing with __str__ method
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def __str__(self):
+        return f"{self.name}({self.age})"
+p1 = Person("John", 36)
+print(p1)
+
+# Method inside class
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def myfunc(self):
+        print("Hello my name is " + self.name)
+p1 = Person("John", 36)
+p1.myfunc()
+
+# Custom names for self
+class Person:
+    def __init__(mysillyobject, name, age):
+        mysillyobject.name = name
+        mysillyobject.age = age
+
+    def myfunc(abc):
+        print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+# Modifying object properties
+p1.age = 40
+
+# Deleting object properties and object
+del p1.age
+del p1
+
+# Empty class
+class Person:
+    pass
