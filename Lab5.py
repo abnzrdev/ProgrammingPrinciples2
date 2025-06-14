03d229e6b1a7f7e467ef9edccc6b4a88509db425
Abenezer <abnzr.dev@gmail.com>
2025-06-14 20:44:40 +0500

    Add functions to find sequences of lowercase and uppercase letters in Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -26,3 +26,17 @@ def match_a_followed_by_anything_ending_in_b(s):
 
 print(match_a_followed_by_anything_ending_in_b("a quick brown fox jumps over the lazy dog b"))  # True
 print(match_a_followed_by_anything_ending_in_b("a quick brown fox"))  # False
+
+# Find sequences of lowercase letters joined with an underscore
+def find_lowercase_with_underscore(s):
+    pattern = r"[a-z]+(_[a-z]+)+"
+    return re.findall(pattern, s)
+
+print(find_lowercase_with_underscore("hello_world_this_is_python"))  # ['hello_world', 'world_this', 'this_is']
+
+# Find sequences of uppercase letter followed by lowercase letters
+def find_uppercase_followed_by_lowercase(s):
+    pattern = r"[A-Z][a-z]+"
+    return re.findall(pattern, s)
+
+print(find_uppercase_followed_by_lowercase("Hello World"))  # ['Hello', 'World']
